4ad8db73e06ca31395956a58ef92be4e0c6fce35
memacs <boyofmonk@gmail.com>
2019-05-06 16:27:43 +0800

    删除打印密码的日志

--- src/qtalk_auth.erl
+++ src/qtalk_auth.erl
@@ -28,15 +28,10 @@ check_user_password(Host, User, Password) ->
                         NewKey = qtalk_public:concat(User,<<"@">>,Host),
                         catch set_user_mac_key(Host,NewKey,Key)
                     end,
-                    ?INFO_MSG("the auth info: ~p~n", [{Password1,Pass, Salt}]),
                     do_check_host_user(Password1,Pass, Salt);
                _ -> false
            end;
         _ -> false
-    end,
-
-    if R =:= false -> ?ERROR_MSG("the auth info is ~p~n", [{Host,User,Password}]);
-    true -> ok
     end,
 
     R.
